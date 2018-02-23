0576c9e80160b054eea90fdbd345f8af8656d7e6
Markus GH <markus.ghambari@gmail.com>
2018-02-24 09:12:36 +1100

    Update patch-0.2.sh

--- patches/patch-0.2.sh
+++ patches/patch-0.2.sh
@@ -1,8 +1,8 @@
 # Check smartcash is running every 5 minutes instead of 1 minute
-(crontab -l | sed -e 's/*\/1 \* \* \* \*/*\/5 \* \* \* \*/g') | crontab -
+crontab -l | sed 's/.*makerun.sh/\*\/5 \* \* \* \* ~\/smartnode\/makerun.sh/g' | crontab -
 
-# Check if upgrade is available every day instead of every 2 hours
-(crontab -l | sed -e 's/*\/120 \* \*/0 0 \*\/1/g') | crontab -
+# Check if upgrade is available every two hours
+crontab -l | sed 's/.*upgrade.sh/0 *\/2 \* \* \* ~\/smartnode\/upgrade.sh/g' | crontab -
 
 # Add a cronjob to start smartcash after reboot
 if ! crontab -l | grep "@reboot smartcashd"; then
@@ -13,6 +13,3 @@ fi
 cd ~/smartnode
 wget https://raw.githubusercontent.com/SmartCash/smartnode/master/makerun.sh -O makerun.sh
 wget https://raw.githubusercontent.com/SmartCash/smartnode/master/upgrade.sh -O upgrade.sh
-
-# Run upgrade.sh to upgrade to the latest version: v1.1.1
-./upgrade.sh
