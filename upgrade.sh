1bff6dafbc28c3cd391bab0c04b7732a900025dd
Markus GH <markus.ghambari@gmail.com>
2018-02-23 11:11:40 +1100

    Update upgrade.sh

--- upgrade.sh
+++ upgrade.sh
@@ -2,11 +2,13 @@
 # upgrade.sh
 # Make sure smartcash is up-to-date
 # Add the following to the crontab (i.e. crontab -e)
-# */120 * * * * ~/smartnode/upgrade.sh
+# 0 0 */1 * * ~/smartnode/upgrade.sh
+
+apt update
 
 if apt list --upgradable | grep -v grep | grep smartcashd > /dev/null
 then
-  smartcash-cli stop && sleep 10 && apt update && apt install smartcashd -y && smartcash-cli stop && sleep 10 && smartcashd
+  smartcash-cli stop && sleep 10 && apt install smartcashd -y && smartcashd
 else
   exit
 fi
