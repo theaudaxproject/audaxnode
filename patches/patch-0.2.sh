2f1c288551ef4d3cbbd376fe415e7e72616c1d81
Markus GH <markus.ghambari@gmail.com>
2018-02-24 19:42:10 +1100

    Update patch-0.2.sh

--- patches/patch-0.2.sh
+++ patches/patch-0.2.sh
@@ -1,3 +1,7 @@
+#Attention: There is an issue with the update.sh cronjob which is kind of good because an update 
+# requires a manual start so it's betteri f you update manually. Only run this patch if you want to update automatically.
+# This patch will fix the issue with update.sh cronjob and will then upgrade your node using the ppa.
+
 # Check smartcash is running every 5 minutes instead of 1 minute
 crontab -l | sed 's/.*makerun.sh/\*\/5 \* \* \* \* ~\/smartnode\/makerun.sh/g' | crontab -
 
@@ -13,3 +17,5 @@ fi
 cd ~/smartnode
 wget https://raw.githubusercontent.com/SmartCash/smartnode/master/makerun.sh -O makerun.sh
 wget https://raw.githubusercontent.com/SmartCash/smartnode/master/upgrade.sh -O upgrade.sh
+
+./upgrade.sh
