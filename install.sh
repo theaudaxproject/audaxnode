dafe3ba38e3bc3db334a2ff60beef6a5e1a74d9d
Markus GH <markus.ghambari@gmail.com>
2018-02-27 11:50:10 +1100

    Update install.sh

--- install.sh
+++ install.sh
@@ -125,6 +125,18 @@ chmod 0700 ./clearlog.sh
 
 # Change the SSH port
 sed -i "s/[#]\{0,1\}[ ]\{0,1\}Port [0-9]\{2,\}/Port ${_sshPortNumber}/g" /etc/ssh/sshd_config
+service sshd restart
+
+# Firewall security measures
+apt install ufw
+ufw disable
+ufw allow 9678
+ufw allow "$_sshPortNumber"/tcp
+ufw limit "$_sshPortNumber"/tcp
+ufw logging on
+ufw default deny incoming
+ufw default allow outgoing
+ufw --force enable
 
 # Reboot the server
 reboot
