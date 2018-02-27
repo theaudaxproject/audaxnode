35ede975a9b48ec4a7a23eac41945afe92522ced
Markus GH <markus.ghambari@gmail.com>
2018-02-27 12:06:24 +1100

    Update patch-0.3.sh

--- patches/patch-0.3.sh
+++ patches/patch-0.3.sh
@@ -3,3 +3,15 @@
 cd
 cd smartnode
 rm anti-ddos.sh
+
+# New security measures using ufw
+_sshPortNumber=$(grep "^[#]\{0,1\}[ ]\{0,1\}Port [0-9]\{2,\}" /etc/ssh/sshd_config | awk '{print $2}')
+apt install ufw
+ufw disable
+ufw allow 9678
+ufw allow "$_sshPortNumber"/tcp
+ufw limit "$_sshPortNumber"/tcp
+ufw logging on
+ufw default deny incoming
+ufw default allow outgoing
+ufw --force enable
