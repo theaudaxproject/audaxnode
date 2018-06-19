b20ac2f374dab6a4ecf54bc828772a5e8105b390
cryptotronxyz <40378575+cryptotronxyz@users.noreply.github.com>
2018-06-18 23:03:33 -0600

    Update patch-0.3.sh

--- patches/patch-0.3.sh
+++ patches/patch-0.3.sh
@@ -1,13 +1,8 @@
-# The anti-ddos script was not working properly so decided to remove it
-cd
-cd smartnode
-rm anti-ddos.sh
-
 # New security measures using ufw
 _sshPortNumber=$(grep "^[#]\{0,1\}[ ]\{0,1\}Port [0-9]\{2,\}" /etc/ssh/sshd_config | awk '{print $2}')
 apt install ufw -y
 ufw disable
-ufw allow 9678
+ufw allow 9009
 ufw allow "$_sshPortNumber"/tcp
 ufw limit "$_sshPortNumber"/tcp
 ufw logging on
