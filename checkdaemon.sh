25c9fe5206392172a90c26dcda974a63ee81a500
cryptotronxyz <40378575+cryptotronxyz@users.noreply.github.com>
2018-06-19 00:13:30 -0600

    Update checkdaemon.sh

--- checkdaemon.sh
+++ checkdaemon.sh
@@ -10,8 +10,8 @@ currentBlock=$(helium-cli getblockcount)
 helium-cli getblockcount > ~/heliumnode/blockcount
 
 if [ "$previousBlock" == "$currentBlock" ]; then
-  cd ~/helium/src
-  ./helium-cli stop
-  sleep 10
-  ./heliumd -daemon
+  cd ~/helium/src;
+  ./helium-cli stop;
+  sleep 10;
+  ./heliumd -daemon;
 fi
