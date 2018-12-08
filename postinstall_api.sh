d4054b7786022c762e361062b4e03760fdcfcfcb
cryptotronxyz <40378575+cryptotronxyz@users.noreply.github.com>
2018-12-07 20:20:22 -0700

    Update postinstall_api.sh

--- postinstall_api.sh
+++ postinstall_api.sh
@@ -1,9 +1,9 @@
 #!/bin/bash
 
-#if [ -e /root/vpsvaletreboot.txt ]; then
+if [ -e /root/vpsvaletreboot.txt ]; then
     #hname=$(</root/installtemp/vpshostname.info)
     hname=$(hostname)
     curl -X POST https://www.heliumstats.online/code-red/status.php -H 'Content-Type: application/json-rpc' -d '{"hostname":"'"$hname"'","message":"Masternode deployment complete"}'
     rm /root/vpsvaletreboot.txt
     rm -rf /root/installtemp
-#fi
+fi
